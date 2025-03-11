407f754ed668b8fd9936c5a64ed18d527663632b
ahmed sadanye <you@example.com>
2025-03-11 08:01:38 +0200

    First Commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+print ("Hello, Ahmed Sadany in Python World")
+print ("Hello, Doha Sadany in Python World")
+print ("Hello, Moaz Sadany in Python World")
+
+print ("Hello, Mona Sadany in Python World")
+
