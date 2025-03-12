6bbc43e9ef22b6c26afb0017c1982cb0f93a8ed1
sadany-8 <50889277+sadany-8@users.noreply.github.com>
2025-03-12 07:22:48 +0200

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-print ("Hello, Ahmed Sadany in Python World")
-print ("Hello, Doha Sadany in Python World")
-print ("Hello, Moaz Sadany in Python World")
-
-print ("Hello, Mona Sadany in Python World")
-
